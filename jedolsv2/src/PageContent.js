3252a4a4c43dcf9a00d6ab5f50e5cc2be5db5662
NaKeR <122722654+NaKeRNarolino@users.noreply.github.com>
2023-10-13 13:23:32 +0400

    Delete jedolsv2 directory

--- jedolsv2/src/PageContent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function PageContent() {
-    return (
-        <div className="page-content">
-            <h1 className="title-heading">сборки и аддоны для minecraft bedrock.</h1>
-        </div>
-    )
-}
--- jedolsv2/src/jButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function JButton({text, onClick}) {
-    return(
-        <button className="jbutton" onClick={onClick}>
-            <p>{text}</p>
-        </button>
-    )
-}
